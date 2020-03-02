076e242661052e4007b2d791dffc6e34d0392381
Marko Cekic <cekicm021@gmail.com>
2020-03-02 15:12:24 +0100

    Added swagger, wired up testing endpoints

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const swaggerUi = require("swagger-ui-express");
+// const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerDocument = require("./swagger.json");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+// TODO : Wire up swaggerJSDoc with real endpoints
